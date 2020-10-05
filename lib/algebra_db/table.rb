e7add671cd6374bb17656d1537574cdf66f69479
Anthony Super <anthony@noided.media>
2020-10-04 20:59:22 -0600

    Additional docs, github actions

--- lib/algebra_db/table.rb
+++ lib/algebra_db/table.rb
@@ -7,13 +7,21 @@ module AlgebraDB
   # Instead, use the *class* in a syntax runner.
   class Table
     class << self
+      ##
+      # Hash of column_name -> column_type
+      #
+      # This returns a value modified with {#dup}, so modifying it
+      # will have no effect on the table defintion.
       def columns
         (@columns || {}).dup
       end
 
+      ##
+      # We customize the inspect method to return a quick defintion of this table.
+      # This makes working with it in a REPL much, much easier.
       def inspect
-        str = "#<#{self.name}:#{self.object_id} "
-        str << "columns=[#{self.columns.keys.map(&:inspect).join(", ")}]>"
+        str = "#<#{name}:#{object_id} "
+        str << "columns=[#{columns.keys.map(&:inspect).join(', ')}]>"
       end
 
       def column(name, value)
@@ -25,6 +33,8 @@ module AlgebraDB
         end
       end
 
+      ##
+      # Does this table contain this column?
       def column?(name)
         columns.key?(name.to_sym)
       end
@@ -34,6 +44,7 @@ module AlgebraDB
       end
 
       def relationship(name, other_table, &block)
+        (@relationships ||= {})[name] = other_table
         define_method(name) do
           relater_proc =
             if block.arity == 2
@@ -45,6 +56,16 @@ module AlgebraDB
         end
       end
 
+      ##
+      # Determine the relationships defined on this table.
+      # This is a pretty simple hash of the name of the relationship to the related table.
+      # It does not include any information on *how* to obtain that relationship: you need to use
+      # the defined instance method on a table instance to get that.
+      attr_reader :relationships
+      ##
+      # The name of this table in Postgres-land.
+      # This is anything you want, and we don't default this.
+      # TODO: maybe default this to follow some kind of rails conventions?
       attr_accessor :table_name
     end
 
